9559f3c0eb67b8f9619aaf79c2820d50299dec07
zaneliu <hi@zaneliu.me>
2025-07-25 19:30:32 +0800

    refactor: 优化若干配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
+import { defineConfig } from 'astro/config'
 import Unocss from 'unocss/astro'
 import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable Solid to support Solid JSX components.
   integrations: [solid(), Unocss()],
   vite: {
     plugins: [
--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import { antfu } from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   solid: true,
+  astro: true,
   markdown: false,
   rules: {
     'no-console': 'off',
